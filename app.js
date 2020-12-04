e466c0d37f7a4155c2d423c2a5f9e82c952f445a
Inggy <71790092+zengingrid@users.noreply.github.com>
2020-12-04 12:24:17 -0800

    clicking on note's title now displays the saved content, hooray

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const header = document.querySelector (".header");
 const footer = document.querySelector (".footer");
 const container = document.querySelector (".container");
 const nav = document.querySelector(".nav");
-const notearea = document.getElementById("notearea");
+var notearea = document.getElementById("notearea");
 const newBtn = document.getElementById("new");
 const saveBtn = document.getElementById("save");
 const darkBtn = document.getElementById("dark");
@@ -12,7 +12,7 @@ let state = 0;
 function changeTheme() {
     if (state == 0) {
         document.body.style.backgroundColor = "#254B62";
-        header.style.backgroundColor = "#1D3E53";
+        header.style.backgroundColor = "#1D3E53"; 
         footer.style.backgroundColor = "#1D3E53";
         nav.style.backgroundColor = "#1D3E53";
         newBtn.style.backgroundColor = "#9E9E9E";
@@ -61,7 +61,7 @@ function newNote() {
 }
 
 
- const notesArray = [
+var notesArray = [
      { title: "note one", body: "some text 1"},
      { title: "note two", body: "some text 2"}
 ]
@@ -70,15 +70,15 @@ function newNote() {
 const ul = document.getElementById("ul");
 
 function saveNote(notesArray) {
-    listLength = notearea.value.split(".").length;
-    title = notearea.value.split(".")[0];
-    body= notearea.value.split(".").splice(1,listLength);
+    listLength = notearea.value.split("\n").length;
+    title = notearea.value.split("\n")[0];
+    body= notearea.value.split("\n").splice(1,listLength);
     newObject = {title: `${title}`, body: `${body}`};
     notesArray.push(newObject);
     ul.insertAdjacentHTML('beforeend', `<li> ${title}</li>`)
 }
 
 ul.addEventListener ('click', (e) => {
-    const targetNote = notesArray.find(n => n["title"] == e.target.innerText)
-    notearea.innerHTML = targetNote["body"];
+     const targetNote = notesArray.find(n => n["title"] == e.target.innerText)
+     notearea.value = targetNote["body"];
 })
